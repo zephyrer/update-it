283eae801e337e17670d6ff3ff631082405fc152
Elijah <Elijah@5557f4c3-8b41-0410-a5a5-81f158d74672>
2006-10-27 06:28:31 +0000

    Corrected setup and build scripts.

--- trunk/makeall.cmd
+++ trunk/makeall.cmd
@@ -2,8 +2,8 @@
 echo.
 echo Building executable - "Win32 MBCS Debug"...
 echo.
-devenv.com UpdateIt70.sln /rebuild MBCS_Debug /project UpdateIt /nologo
+devenv.com UpdateIt71.sln /rebuild MBCS_Debug /project UpdateIt /nologo
 echo Building executable - "Win32 MBCS Release"...
 echo.
-devenv.com UpdateIt70.sln /rebuild MBCS_Release /project UpdateIt /nologo
+devenv.com UpdateIt71.sln /rebuild MBCS_Release /project UpdateIt /nologo
 echo.
