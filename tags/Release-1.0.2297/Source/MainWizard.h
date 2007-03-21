8d9e15c90a1ae888ac3f6084ab799c84dbf552ca
elijah <elijah@5557f4c3-8b41-0410-a5a5-81f158d74672>
2007-03-21 15:00:07 +0000

    Tagged release 1.0.2297.

---
+++ tags/Release-1.0.2297/Source/MainWizard.h
@@ -0,0 +1,53 @@
+// UpdateIt! application.
+// Copyright (c) 2002-2004 by Elijah Zarezky,
+// All rights reserved.
+
+// MainWizard.h - interface of the CMainWizard class
+
+#if !defined(__MainWizard_h)
+#define __MainWizard_h
+
+class CMainWizard: public CCustomPropSheet
+{
+	DECLARE_DYNAMIC(CMainWizard)
+	DECLARE_MESSAGE_MAP()
+
+// construction/destruction
+public:
+	CMainWizard(void);
+	virtual ~CMainWizard(void);
+
+// overridables
+public:
+	virtual BOOL OnInitDialog(void);
+
+// attributes
+private:
+	HICON m_hIcon;
+	HICON m_hSmIcon;
+	enum {
+		// page indices
+		I_ABOUT,
+		I_OPTIONS,
+		I_FILES,
+		I_ACTION,
+		I_PROGRESS,
+		NUM_PAGES		// should be the LAST enumerator!
+	};
+	CAboutPage m_pageAbout;
+	COptionsPage m_pageOptions;
+	CFilesPage m_pageFiles;
+	CActionPage m_pageAction;
+	CProgressPage m_pageProgress;
+
+// diagnostic services
+#if defined(_DEBUG)
+public:
+	virtual void AssertValid(void) const;
+	virtual void Dump(CDumpContext& dumpCtx) const;
+#endif
+};
+
+#endif	// __MainWizard_h
+
+// end of file
---
+++ tags/Release-1.0.2297/Source/stdafx.h
@@ -0,0 +1,74 @@
+// UpdateIt! application.
+// Copyright (c) 2002-2004 by Elijah Zarezky,
+// All rights reserved.
+
+// stdafx.h - include file for standard system include files
+
+#if !defined(__stdafx_h)
+#define __stdafx_h
+
+// unreferenced inline/local function has been removed
+#pragma warning(disable: 4514)
+// function not inlined
+#pragma warning(disable: 4710)
+// identifier was truncated in the debug information
+#pragma warning(disable: 4786)
+
+// use Win98/NT4 features
+#define _WIN32_WINNT 0x0400
+
+// MFC headers
+#include <afxwin.h>			// MFC core and standard components
+#include <afxdlgs.h>			// MFC standard dialogs
+#include <afxdtctl.h>		// MFC date/time control classes
+#include <afxcmn.h>			// MFC common control classes
+#include <afxpriv.h>			// MFC private classes
+#include <afxsock.h>			// MFC support for Windows Sockets
+#include <afxinet.h>			// MFC support for WinInet
+
+// PSDK headers
+#include <shlwapi.h>			// light-weight utility APIs
+#include <wincrypt.h>		// cryptographic API prototypes and definitions
+
+// STL headers
+#pragma warning(push, 3)
+#include <string>
+#if !defined(_STRING_)
+#define _STRING_			// for compatibility with Dinkumware STL
+#endif
+#include <algorithm>
+using std::min;
+using std::max;
+#pragma warning(pop)
+
+// CRT headers
+#include <sys/utime.h>
+#include <locale.h>
+
+// additional headers
+#include "../../Repository/CodeProject/Source/HyperLink.h"
+#include "../../Repository/CodeProject/Source/FolderDlg.h"
+#pragma warning(push)
+#include "../../Repository/ZipArchive/Source/ZipArchive.h"
+#pragma warning(pop)
+#include "../../Repository/AfxGadgets/Source/SortingListCtrl.h"
+#include "../../Repository/AfxGadgets/Source/WinCrypto.h"
+#include "../../Repository/AfxGadgets/Source/Win32Error.h"
+#include "../../Repository/AfxGadgets/Source/StringConv.h"
+#include "../../Repository/AfxGadgets/Source/MemMapFile.h"
+#include "../../Repository/Naughter/Source/Smtp.h"
+
+#pragma hdrstop
+
+// file data
+struct FILE_DATA {
+	TCHAR szName[_MAX_FNAME];
+	TCHAR szExt[_MAX_EXT];
+	TCHAR szFolder[_MAX_DIR];
+	CTime timeWrite;
+	DWORD cbLength;
+};
+
+#endif	// __stdafx_h
+
+// end of file
