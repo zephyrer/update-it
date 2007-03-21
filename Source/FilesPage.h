c3fdd99990a0067da7f303c5d76fb0ab69cee935
elijah <elijah@5557f4c3-8b41-0410-a5a5-81f158d74672>
2007-03-21 15:05:13 +0000

    Tagged release 1.0.2777.

--- Source/FilesPage.h
+++ Source/FilesPage.h
@@ -1,18 +1,6 @@
 // UpdateIt! application.
-// Copyright (c) 2002-2006 by Elijah Zarezky,
+// Copyright (c) 2002-2005 by Elijah Zarezky,
 // All rights reserved.
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 // FilesPage.h - interface of the CFilesPage class
 
@@ -55,11 +43,7 @@ public:
 	CStatic m_textInfo;
 	CFilesList m_listFiles;
 	CButton m_buttonRemove;
-#if (_MFC_VER < 0x0700)
 	DWORD m_cbFiles;
-#else
-	ULONGLONG m_cbFiles;
-#endif	// _MFC_VER
 	CStringList m_listExclude;
 	CImageList m_imageList;
 	CMap<CString, LPCTSTR, int, int> m_mapIcons;
