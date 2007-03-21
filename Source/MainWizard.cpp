bd448b8c2c3565372ef545d375b6296b0be0d5e4
elijah <elijah@5557f4c3-8b41-0410-a5a5-81f158d74672>
2007-03-21 15:00:07 +0000

    Tagged release 1.0.2297.

--- Source/MainWizard.cpp
+++ Source/MainWizard.cpp
@@ -1,42 +1,21 @@
 // UpdateIt! application.
-// Copyright (c) 2002-2006 by Elijah Zarezky,
+// Copyright (c) 2002-2004 by Elijah Zarezky,
 // All rights reserved.
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 // MainWizard.cpp - implementation of the CMainWizard class
 
 #include "stdafx.h"
-
 #include "Resource.h"
 #include "BetterPropPage.h"
 #include "AboutPage.h"
 #include "OptionsPage.h"
 #include "FilesList.h"
 #include "FilesPage.h"
-#include "CustomDialog.h"
-#include "AuthenticationDialog.h"
 #include "ActionPage.h"
 #include "ProgressPage.h"
 #include "CustomPropSheet.h"
 #include "MainWizard.h"
 #include "UpdateItApp.h"
-#include "Registry.h"
-
-#if defined(__INTEL_COMPILER)
-// remark #171: invalid type conversion
-#pragma warning(disable: 171)
-#endif	// __INTEL_COMPILER
 
 #if defined(_DEBUG)
 #undef THIS_FILE
@@ -49,11 +28,7 @@ IMPLEMENT_DYNAMIC(CMainWizard, CCustomPropSheet)
 
 // message map
 BEGIN_MESSAGE_MAP(CMainWizard, CCustomPropSheet)
-	ON_WM_INITMENUPOPUP()
-	ON_WM_SYSCOMMAND()
 END_MESSAGE_MAP()
-
-// construction/destruction
 
 CMainWizard::CMainWizard(void):
 CCustomPropSheet(AFX_IDS_APP_TITLE)
@@ -62,15 +37,13 @@ CCustomPropSheet(AFX_IDS_APP_TITLE)
 	ASSERT_VALID(pApp);
 
 	// assign CRT locale
-	static const TCHAR szDefLocale[] = _T("English_USA.1252");
-	_tsetlocale(LC_ALL, pApp->GetProfileString(SZ_REGK_LOCALE, SZ_REGV_LOCALE_LC_ALL, szDefLocale));
+	_tsetlocale(LC_ALL, pApp->GetProfileString(_T("Locale"), _T("LC_ALL"), _T("English_USA.1252")));
 
 	// load dialog's icons
 	m_hIcon = pApp->LoadIcon(IDI_APP_ICON);
 	m_hSmIcon = pApp->LoadSmIcon(MAKEINTRESOURCE(IDI_APP_ICON));
 
-	static HYPERLINKCOLORS linkColors =
-	{
+	static HYPERLINKCOLORS linkColors = {
 		RGB(0, 0, 255),	// default
 		RGB(0, 0, 255),	// active
 		RGB(0, 0, 255),	// visited
@@ -92,20 +65,16 @@ CMainWizard::~CMainWizard(void)
 	::DestroyIcon(m_hIcon);
 }
 
-// overridables
-
 BOOL CMainWizard::OnInitDialog(void)
 {
-	CString strRestore;
 	CString strMinimize;
 	CMenu menuTemp;
-	CString strNewItem;
 	int nInitialDelay, nAutoPopDelay;
 	int cxMaxWidth;
 	DWORD crTipBk, crTipText;
 
 	// invoke inherited handler
-	BOOL fResult = __super::OnInitDialog();
+	BOOL fResult = CCustomPropSheet::OnInitDialog();
 
 	// set wizard's icons
 	SetIcon(m_hIcon, TRUE);
@@ -113,53 +82,31 @@ BOOL CMainWizard::OnInitDialog(void)
 
 	// try to obtain localized text for the "Minimize" system command
 	HMODULE hUser32 = ::GetModuleHandle(_T("user32"));
-	if (menuTemp.Attach(::LoadMenu(hUser32, MAKEINTRESOURCE(16))))
-	{
-		menuTemp.GetMenuString(SC_RESTORE, strRestore, MF_BYCOMMAND);
+	if (menuTemp.Attach(::LoadMenu(hUser32, MAKEINTRESOURCE(16)))) {
 		menuTemp.GetMenuString(SC_MINIMIZE, strMinimize, MF_BYCOMMAND);
 		::DestroyMenu(menuTemp.Detach());
 	}
-	if (strMinimize.IsEmpty())
-	{
+	if (strMinimize.IsEmpty()) {
 		// probably fuckin' Win9x
-		strRestore.LoadString(IDS_SC_RESTORE);
 		strMinimize.LoadString(IDS_SC_MINIMIZE);
 	}
 
 	// adjust system menu
 	CMenu* pSysMenu = GetSystemMenu(FALSE);
 	ASSERT_VALID(pSysMenu);
-	pSysMenu->InsertMenu(SC_MOVE, MF_BYCOMMAND, SC_RESTORE, strRestore);
 	pSysMenu->InsertMenu(SC_CLOSE, MF_BYCOMMAND, SC_MINIMIZE, strMinimize);
 	pSysMenu->InsertMenu(SC_CLOSE, MF_BYCOMMAND | MF_SEPARATOR);
-	MENUITEMINFO miInfo = { sizeof(miInfo), MIIM_BITMAP };
-	miInfo.hbmpItem = HBMMENU_POPUP_RESTORE;
-	::SetMenuItemInfo(pSysMenu->GetSafeHmenu(), SC_RESTORE, FALSE, &miInfo);
-	miInfo.hbmpItem = HBMMENU_POPUP_MINIMIZE;
-	::SetMenuItemInfo(pSysMenu->GetSafeHmenu(), SC_MINIMIZE, FALSE, &miInfo);
 	ModifyStyle(0, WS_MINIMIZEBOX);
-	OSVERSIONINFO osVerInfo = { sizeof(osVerInfo) };
-	GetVersionEx(&osVerInfo);
-	if (osVerInfo.dwPlatformId == VER_PLATFORM_WIN32_NT && osVerInfo.dwMajorVersion >= 5)
-	{
-		// Windows 2000/XP
-		strNewItem.LoadString(IDS_SC_EXPORT_SETTINGS);
-		pSysMenu->InsertMenu(0, MF_BYPOSITION, IDM_SC_EXPORT_SETTINGS, strNewItem);
-		strNewItem.LoadString(IDS_SC_IMPORT_SETTINGS);
-		pSysMenu->InsertMenu(1, MF_BYPOSITION, IDM_SC_IMPORT_SETTINGS, strNewItem);
-		pSysMenu->InsertMenu(2, MF_BYPOSITION | MF_SEPARATOR);
-	}
 
 	// customize tool tips
 	CWinApp* pApp = AfxGetApp();
-	nInitialDelay = pApp->GetProfileInt(SZ_REGK_TIPS, SZ_REGV_TIPS_INITIAL_DELAY, 900);
-	nAutoPopDelay = pApp->GetProfileInt(SZ_REGK_TIPS, SZ_REGV_TIPS_AUTO_POP_DELAY, 5000);
-	cxMaxWidth = pApp->GetProfileInt(SZ_REGK_TIPS, SZ_REGV_TIPS_MAX_WIDTH, 300);
-	crTipBk = pApp->GetProfileInt(SZ_REGK_TIPS, SZ_REGV_TIPS_BK_COLOR, ::GetSysColor(COLOR_INFOBK));
-	crTipText = pApp->GetProfileInt(SZ_REGK_TIPS, SZ_REGV_TIPS_TEXT_COLOR, ::GetSysColor(COLOR_INFOTEXT));
+	nInitialDelay = pApp->GetProfileInt(_T("Tips"), _T("InitialDelay"), 900);
+	nAutoPopDelay = pApp->GetProfileInt(_T("Tips"), _T("AutoPopDelay"), 5000);
+	cxMaxWidth = pApp->GetProfileInt(_T("Tips"), _T("MaxWidth"), 300);
+	crTipBk = pApp->GetProfileInt(_T("Tips"), _T("BkColor"), ::GetSysColor(COLOR_INFOBK));
+	crTipText = pApp->GetProfileInt(_T("Tips"), _T("TextColor"), ::GetSysColor(COLOR_INFOTEXT));
 	int cPages = m_pages.GetSize();
-	for (int i = 0; i < cPages; ++i)
-	{
+	for (int i = 0; i < cPages; ++i) {
 		CBetterPropPage* pPage = reinterpret_cast<CBetterPropPage*>(m_pages[i]);
 		CToolTipCtrl& tipWnd = pPage->GetToolTipCtrl();
 		tipWnd.SetDelayTime(TTDT_INITIAL, nInitialDelay);
@@ -173,97 +120,6 @@ BOOL CMainWizard::OnInitDialog(void)
 	return (fResult);
 }
 
-// message map functions
-
-void CMainWizard::OnInitMenuPopup(CMenu* pPopupMenu, UINT uIndex, BOOL fSysMenu)
-{
-	__super::OnInitMenuPopup(pPopupMenu, uIndex, fSysMenu);
-	if (fSysMenu)
-	{
-		ASSERT_VALID(pPopupMenu);
-		UINT fuEnable = MF_BYCOMMAND | (GetActiveIndex() > 0 ? MF_GRAYED : MF_ENABLED);
-		pPopupMenu->EnableMenuItem(IDM_SC_IMPORT_SETTINGS, fuEnable); 
-	}
-}
-
-void CMainWizard::OnSysCommand(UINT uID, LPARAM lParam)
-{
-	switch (uID & 0xFFF0)
-	{
-	case IDM_SC_EXPORT_SETTINGS:
-		OnScExportSettings();
-		break;
-	case IDM_SC_IMPORT_SETTINGS:
-		OnScImportSettings();
-		break;
-	default:
-		__super::OnSysCommand(uID, lParam);
-		break;
-	}
-}
-
-// implementation helpers
-
-void CMainWizard::OnScExportSettings(void)
-{
-	CString strFilter;
-	CString strTitle;
-	CProcessPrivileges processPrivileges;
-
-	enum { fdwFlags = OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY };
-	strFilter.LoadString(IDS_SETTINGS_FILTER);
-#if (_MFC_VER < 0x0700)
-	CFileDialogEx dlgSaveAs(FALSE, _T("hive"), AfxGetAppName(), fdwFlags, strFilter);
-#else
-	CFileDialog dlgSaveAs(FALSE, _T("hive"), AfxGetAppName(), fdwFlags, strFilter);
-#endif	// _MFC_VER
-	strTitle.LoadString(IDS_TITLE_EXPORT);
-	dlgSaveAs.m_ofn.lpstrTitle = strTitle;
-	if (dlgSaveAs.DoModal() == IDOK)
-	{
-		BeginWaitCursor();
-		CWinApp* pApp = AfxGetApp();
-		ASSERT_VALID(pApp);
-		processPrivileges[SE_BACKUP_NAME] = SE_PRIVILEGE_ENABLED;
-		HKEY hAppKey = pApp->GetAppRegistryKey();
-		::RegSaveKey(hAppKey, dlgSaveAs.GetPathName(), NULL);
-		::RegCloseKey(hAppKey);
-		processPrivileges[SE_BACKUP_NAME] = 0;
-		EndWaitCursor();
-	}
-}
-
-void CMainWizard::OnScImportSettings(void)
-{
-	CString strFilter;
-	CString strTitle;
-	CProcessPrivileges processPrivileges;
-
-	enum { fdwFlags = OFN_HIDEREADONLY | OFN_PATHMUSTEXIST };
-	strFilter.LoadString(IDS_SETTINGS_FILTER);
-#if (_MFC_VER < 0x0700)
-	CFileDialogEx dlgOpen(TRUE, _T("hive"), AfxGetAppName(), fdwFlags, strFilter);
-#else
-	CFileDialog dlgOpen(TRUE, _T("hive"), AfxGetAppName(), fdwFlags, strFilter);
-#endif	// _MFC_VER
-	strTitle.LoadString(IDS_TITLE_IMPORT);
-	dlgOpen.m_ofn.lpstrTitle = strTitle;
-	if (dlgOpen.DoModal() == IDOK)
-	{
-		BeginWaitCursor();
-		CWinApp* pApp = AfxGetApp();
-		ASSERT_VALID(pApp);
-		processPrivileges[SE_RESTORE_NAME] = SE_PRIVILEGE_ENABLED;
-		HKEY hAppKey = pApp->GetAppRegistryKey();
-		::RegRestoreKey(hAppKey, dlgOpen.GetPathName(), REG_FORCE_RESTORE);
-		::RegCloseKey(hAppKey);
-		processPrivileges[SE_RESTORE_NAME] = 0;
-		EndWaitCursor();
-	}
-}
-
-// diagnostic services
-
 #if defined(_DEBUG)
 
 //! This member function performs a validity check on this object by checking its
@@ -274,8 +130,7 @@ void CMainWizard::OnScImportSettings(void)
 void CMainWizard::AssertValid(void) const
 {
 	// first perform inherited validity check...
-	__super::AssertValid();
-
+	CCustomPropSheet::AssertValid();
 	// ...and then verify own state as well
 	ASSERT_VALID(&m_pageAbout);
 	ASSERT_VALID(&m_pageOptions);
@@ -290,11 +145,9 @@ void CMainWizard::AssertValid(void) const
 //! @param dumpCtx the diagnostic dump context for dumping, usually afxDump.
 void CMainWizard::Dump(CDumpContext& dumpCtx) const
 {
-	try
-	{
+	try {
 		// first invoke inherited dumper...
-		__super::Dump(dumpCtx);
-
+		CCustomPropSheet::Dump(dumpCtx);
 		// ...and then dump own unique members
 		dumpCtx << "m_hIcon = " << m_hIcon;
 		dumpCtx << "\nm_hSmIcon = " << m_hSmIcon;
@@ -304,8 +157,7 @@ void CMainWizard::Dump(CDumpContext& dumpCtx) const
 		dumpCtx << "\nm_pageAction = " << m_pageAction;
 		dumpCtx << "\nm_pageProgress = " << m_pageProgress;
 	}
-	catch (CFileException* pXcpt)
-	{
+	catch (CFileException* pXcpt) {
 		pXcpt->ReportError();
 		pXcpt->Delete();
 	}
--- Source/OptionsPage.h
+++ Source/OptionsPage.h
@@ -1,27 +1,11 @@
 // UpdateIt! application.
-// Copyright (c) 2002-2006 by Elijah Zarezky,
+// Copyright (c) 2002-2004 by Elijah Zarezky,
 // All rights reserved.
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 // OptionsPage.h - interface of the COptionsPage class
 
 #if !defined(__OptionsPage_h)
 #define __OptionsPage_h
-
-#if defined(_MSC_VER) && (_MSC_VER > 1000)
-#pragma once
-#endif	// _MSC_VER
 
 //! Encapsulates the "Setting options" step of the UpdateIt! wizard.
 class COptionsPage: public CBetterPropPage
