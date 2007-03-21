87341cbe1c5ddc156a9246197070c997711ceb73
elijah <elijah@5557f4c3-8b41-0410-a5a5-81f158d74672>
2007-03-21 14:50:07 +0000

    Tagged release 1.0.2172.1.

--- Source/CustomPropSheet.h
+++ Source/CustomPropSheet.h
@@ -1,28 +1,12 @@
 // UpdateIt! application.
-// Copyright (c) 2002-2006 by Elijah Zarezky,
+// Copyright (c) 2002-2004 by Elijah Zarezky,
 // All rights reserved.
 // Portions copyright (c) 1996-1998 by Microsoft (KB Q142170).
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 // CustomPropSheet.h - interface of the CCustomPropSheet class
 
 #if !defined(__CustomPropSheet_h)
 #define __CustomPropSheet_h
-
-#if defined(_MSC_VER) && (_MSC_VER > 1000)
-#pragma once
-#endif	// _MSC_VER
 
 #define PSM_RESIZE_PAGE (WM_APP + 2)
 
