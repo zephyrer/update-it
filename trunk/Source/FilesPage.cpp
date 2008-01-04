89b424d627b3bcc73589a22c5d1362ff495df545
Elijah Zarezky <Elijah Zarezky@5557f4c3-8b41-0410-a5a5-81f158d74672>
2008-01-04 18:57:44 +0000

    Updated copyright notices.

--- trunk/Source/FilesPage.cpp
+++ trunk/Source/FilesPage.cpp
@@ -1,5 +1,5 @@
 // UpdateIt! application.
-// Copyright (c) 2002-2007 by Elijah Zarezky,
+// Copyright (c) 2002-2008 by Elijah Zarezky,
 // All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
