2b37a1679e674bff11fa6cd2853e9d5213d6d903
Elijah Zarezky <Elijah Zarezky@5557f4c3-8b41-0410-a5a5-81f158d74672>
2008-07-06 11:36:27 +0000

    Improved compatibility with the Visual Studio 2008.

--- Source/stdafx.cpp
+++ Source/stdafx.cpp
@@ -97,7 +97,6 @@ void DDV_MinMaxChars(CDataExchange* pDX, CString const& strValue, int cMinChars,
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.CRT' version='9.0.21022.8' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.MFC' version='9.0.21022.8' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #endif   // _DEBUG
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.MFCLOC' version='9.0.21022.8' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #endif   // _MSC_VER
