8b280853679bf18d0c7a6efc4a2b845344405772
Elijah Zarezky <Elijah Zarezky@5557f4c3-8b41-0410-a5a5-81f158d74672>
2008-09-02 18:57:15 +0000

    Slightly cleaned-up source code.

--- trunk/Languages/Russian_Russia.1251/Source/stdafx.cpp
+++ trunk/Languages/Russian_Russia.1251/Source/stdafx.cpp
@@ -31,8 +31,6 @@ static char THIS_FILE[] = __FILE__;
 //////////////////////////////////////////////////////////////////////////////////////////////
 // manifest dependencies
 
-#if (_MSC_VER == 1500)
-
 #if defined(_DEBUG)
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugCRT' version='9.0.21022.8' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugMFC' version='9.0.21022.8' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
@@ -41,7 +39,5 @@ static char THIS_FILE[] = __FILE__;
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.MFC' version='9.0.21022.8' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #endif   // _DEBUG
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
-#endif   // _MSC_VER
 
 // end of file
