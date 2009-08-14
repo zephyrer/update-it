bc495964558a3685e85945800ef7191064604bfa
Elijah Zarezky <Elijah Zarezky@5557f4c3-8b41-0410-a5a5-81f158d74672>
2009-08-14 10:19:46 +0000

    Started integration with the XCrashReport by Hans Dietrich.

---
+++ Source/CrashFileNames.h
@@ -0,0 +1,38 @@
+// UpdateIt! application.
+// Copyright (c) 2002-2009 by Elijah Zarezky,
+// All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// CrashFileNames.h  Version 1.4
+//
+// Author:  Hans Dietrich
+//          hdietrich2@hotmail.com
+//
+// This software is released into the public domain.
+// You are free to use it in any way you like, except
+// that you may not sell this source code.
+//
+// This software is provided "as is" with no expressed
+// or implied warranty.  I accept no liability for any
+// damage or loss of business that this software may cause.
+
+#ifndef __CRASHFILENAMES_H
+#define __CRASHFILENAMES_H
+
+#define XCRASHREPORT_MINI_DUMP_FILE      _T("CRASH.DMP")
+#define XCRASHREPORT_ERROR_LOG_FILE      _T("ERRORLOG.TXT")
+#define XCRASHREPORT_REGISTRY_DUMP_FILE  _T("REGISTRY.TXT")
+#define XCRASHREPORT_CRASH_REPORT_APP    _T("CrashReporter.exe")
+
+#endif   // __CRASHFILENAMES_H
