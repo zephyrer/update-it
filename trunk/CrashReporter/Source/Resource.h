6a8f5894723444dd8eaee1f4d972404faadb54e6
Elijah Zarezky <Elijah Zarezky@5557f4c3-8b41-0410-a5a5-81f158d74672>
2009-08-15 08:45:25 +0000

    Continued integration with the XCrashReport by Hans Dietrich.

--- trunk/CrashReporter/Source/Resource.h
+++ trunk/CrashReporter/Source/Resource.h
@@ -1,17 +1,18 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by CrashReporter.rc
 //
 #define IDI_APP_ICON                    1
 #define IDD_MAIN                        101
+#define IDB_XLISTCTRL_CHECKBOXES        102
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_SYMED_VALUE           103
 #endif
 #endif
