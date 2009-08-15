6a0fd40bcd7bdb51f8042474b7d35d72a81ee508
Elijah Zarezky <Elijah Zarezky@5557f4c3-8b41-0410-a5a5-81f158d74672>
2009-08-15 09:23:27 +0000

    Continued integration with the XCrashReport by Hans Dietrich.

--- CrashReporter/Source/CrashReporterApp.cpp
+++ CrashReporter/Source/CrashReporterApp.cpp
@@ -29,6 +29,7 @@
 
 #include "CrashReporterApp.h"
 #include "MainDialog.h"
+#include "../../Source/Registry.h"
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // debugging support
@@ -53,7 +54,8 @@ END_MESSAGE_MAP()
 //////////////////////////////////////////////////////////////////////////////////////////////
 // construction/destruction
 
-CCrashReporterApp::CCrashReporterApp(void)
+CCrashReporterApp::CCrashReporterApp(void):
+m_hLangDLL(NULL)
 {
 }
 
@@ -70,6 +72,7 @@ HICON CCrashReporterApp::LoadSmIcon(LPCTSTR pszResName)
 	int cxSmIcon = ::GetSystemMetrics(SM_CXSMICON);
 	int cySmIcon = ::GetSystemMetrics(SM_CYSMICON);
 	HANDLE hSmIcon = ::LoadImage(hInstRes, pszResName, IMAGE_ICON, cxSmIcon, cySmIcon, 0);
+	
 	return (static_cast<HICON>(hSmIcon));
 }
 
@@ -78,10 +81,221 @@ HICON CCrashReporterApp::LoadSmIcon(LPCTSTR pszResName)
 
 BOOL CCrashReporterApp::InitInstance(void)
 {
+#if defined(_DEBUG)
+	for (int i = 0; i < __argc; i++)
+	{
+		TRACE(_T("argv[%i] == %s\n"), i, __targv[i]);
+	}
+#endif   // _DEBUG
+
+	if (__argc < 3)
+	{
+		return (FALSE);
+	}
+	else
+	{
+		m_strUpdateItExe = __targv[1];
+		m_strAppDataPath = __targv[2];
+	}
+
 	CMainDialog dlgMain;
 	m_pMainWnd = &dlgMain;
 	dlgMain.DoModal();
+	
 	return (FALSE);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// implementation helpers
+
+void CCrashReporterApp::GetAbsolutePath(LPTSTR pszDest, LPCTSTR pszRelativePath)
+{
+	if (::PathIsRelative(pszRelativePath))
+	{
+		TCHAR szOurDir[_MAX_PATH] = { 0 };
+		::GetModuleFileName(AfxGetInstanceHandle(), szOurDir, _countof(szOurDir));
+		LPTSTR pszLastSlash = _tcsrchr(szOurDir, _T('\\'));
+		ASSERT(pszLastSlash != NULL);
+		*pszLastSlash = 0;
+		TCHAR szTmpPath[_MAX_PATH] = { 0 };
+		if (pszRelativePath[0] != _T('.'))
+		{
+			_tcscpy(szTmpPath, _T(".\\"));
+			_tcscat(szTmpPath, pszRelativePath);
+			pszRelativePath = szTmpPath;
+		}
+		::PathCombine(pszDest, szOurDir, pszRelativePath);
+	}
+	else {
+		_tcscpy(pszDest, pszRelativePath);
+	}
+}
+
+bool CCrashReporterApp::RegQueryLanguagePath(LPCTSTR pszValueName, LPTSTR pszDest, ULONG cchMaxLen)
+{
+	// precondition
+	ASSERT(pszValueName == NULL || (AfxIsValidString(pszValueName) && *pszValueName != 0));
+	ASSERT(AfxIsValidString(pszDest));
+
+	*pszDest = 0;
+	LONG nError = ERROR_SUCCESS;
+
+	ATL::CRegKey regKeyLangs;
+	regKeyLangs.Attach(GetSectionKey(SZ_REGK_LANGUAGES));
+
+	if (static_cast<HKEY>(regKeyLangs) != NULL)
+	{
+		TCHAR szCurLang[4] = { 0 };
+		ULONG cchLangMax = _countof(szCurLang);
+		nError = regKeyLangs.QueryStringValue(SZ_REGV_LANGUAGES_CURRENT, szCurLang, &cchLangMax);
+		if (nError == ERROR_SUCCESS)
+		{
+			ATL::CRegKey regKeyCurLang;
+			nError = regKeyCurLang.Open(regKeyLangs, szCurLang);
+			if (nError == ERROR_SUCCESS)
+			{
+				nError = regKeyCurLang.QueryStringValue(pszValueName, pszDest, &cchMaxLen);
+				regKeyCurLang.Close();
+			}
+		}
+		::RegCloseKey(regKeyLangs.Detach());
+	}
+
+	return (nError == ERROR_SUCCESS && _tcslen(pszDest) > 0 /*&& ::PathFileExists(pszDest)*/);
+}
+
+#if defined(_DEBUG)
+
+#if (_MSC_VER == 1300)
+#define SZ_DEVENV_VER "2002"
+#elif (_MSC_VER == 1310)
+#define SZ_DEVENV_VER "2003"
+#elif (_MSC_VER == 1400)
+#define SZ_DEVENV_VER "2005"
+#elif (_MSC_VER == 1500)
+#define SZ_DEVENV_VER "2008"
+#else
+#error Unrecognized Development Environment version!
+#endif   // _MSC_VER
+
+#endif   // _DEBUG
+
+bool CCrashReporterApp::GetLanguagePath(LPTSTR pszDest)
+{
+	// precondition
+	ASSERT(AfxIsValidString(pszDest));
+
+	TCHAR szTempPath[_MAX_PATH] = { 0 };
+	if (RegQueryLanguagePath(SZ_REGV_LANG_DLL, szTempPath))
+	{
+#if defined(_DEBUG)
+		TCHAR szExeDir[_MAX_PATH] = { 0 };
+		::GetModuleFileName(AfxGetInstanceHandle(), szExeDir, _countof(szExeDir));
+		::PathRemoveFileSpec(szExeDir);
+		CString strDebugPath(::PathFindFileName(szTempPath));
+		TCHAR szLangDir[_MAX_DIR] = { 0 };
+		_tcscpy(szLangDir, strDebugPath);
+		::PathRemoveExtension(szLangDir);
+		_tcscat(szLangDir, _T("\\Output." SZ_DEVENV_VER "\\x86\\Debug"));
+#if defined(_MBCS)
+		_tcscat(szLangDir, _T("\\MBCS\\"));
+#else
+		_tcscat(szLangDir, _T("\\Unicode\\"));
+#endif   // _MBCS
+		strDebugPath.Insert(0, szLangDir);
+		strDebugPath.Insert(0, _T("..\\..\\..\\..\\..\\Languages\\"));
+		::PathCombine(pszDest, szExeDir, strDebugPath);
+#else
+		GetAbsolutePath(pszDest, szTempPath);
+#endif   // _DEBUG
+		return (true);
+	}
+	else {
+		return (false);
+	}
+}
+
+bool CCrashReporterApp::GetAfxLanguagePath(LPTSTR pszDest)
+{
+	// precondition
+	ASSERT(AfxIsValidString(pszDest));
+
+	TCHAR szTempPath[_MAX_PATH] = { 0 };
+	if (RegQueryLanguagePath(NULL, szTempPath))
+	{
+#if defined(_DEBUG)
+		TCHAR szExeDir[_MAX_PATH] = { 0 };
+		::GetModuleFileName(AfxGetInstanceHandle(), szExeDir, _countof(szExeDir));
+		::PathRemoveFileSpec(szExeDir);
+		CString strRedistPath(::PathFindFileName(szTempPath));
+		strRedistPath.Insert(0, _T("..\\..\\..\\..\\..\\Redist\\Microsoft.VC90.MFCLOC"));
+		::PathCombine(pszDest, szExeDir, strRedistPath);
+#else
+		GetAbsolutePath(pszDest, szTempPath);
+#endif   // _DEBUG
+		return (true);
+	}
+	else {
+		return (false);
+	}
+}
+
+bool CCrashReporterApp::SetCurrentLanguage(void)
+{
+	bool fSuccess = false;
+
+	if (m_hLangDLL != NULL)
+	{
+		::FreeLibrary(m_hLangDLL);
+		m_hLangDLL = NULL;
+	}
+
+	CString strLangPath;
+	if (GetLanguagePath(strLangPath.GetBuffer(_MAX_PATH)))
+	{
+		strLangPath.ReleaseBuffer();
+		m_hLangDLL = ::LoadLibrary(strLangPath);
+		if (m_hLangDLL != NULL)
+		{
+			int iNameStart = strLangPath.ReverseFind(_T('\\'));
+			ASSERT(iNameStart > 0);
+			++iNameStart;
+			int iExtStart = strLangPath.ReverseFind(_T('.'));
+			ASSERT(iExtStart > 0);
+			CString strLocName = strLangPath.Mid(iNameStart, iExtStart - iNameStart);
+			WriteProfileString(SZ_REGK_LOCALE, SZ_REGV_LOCALE_LC_ALL, strLocName);
+			fSuccess = true;
+		}
+	}
+	else {
+		strLangPath.ReleaseBuffer();
+	}
+
+	return (fSuccess);
+}
+
+bool CCrashReporterApp::SetCurrentAfxLanguage(void)
+{
+	bool fSuccess = false;
+
+	TCHAR szAfxLangPath[_MAX_PATH] = { 0 };
+	if (GetAfxLanguagePath(szAfxLangPath))
+	{
+		HINSTANCE hAfxLangDLL = ::LoadLibrary(szAfxLangPath);
+		if (hAfxLangDLL != NULL)
+		{
+			AFX_MODULE_STATE* pState = AfxGetModuleState();
+			ASSERT(pState != NULL);
+			if (pState->m_appLangDLL != NULL)
+			{
+				::FreeLibrary(pState->m_appLangDLL);
+			}
+			pState->m_appLangDLL = hAfxLangDLL;
+			fSuccess = true;
+		}
+	}
+
+	return (fSuccess);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -105,6 +319,9 @@ void CCrashReporterApp::Dump(CDumpContext& dumpCtx) const
 		CWinApp::Dump(dumpCtx);
 
 		// ...and then dump own unique members
+		dumpCtx << "m_hLangDLL = " << m_hLangDLL;
+		dumpCtx << "\nm_strUpdateItExe = " << m_strUpdateItExe;
+		dumpCtx << "\nm_strAppDataPath = " << m_strAppDataPath;
 	}
 	catch (CFileException* pXcpt)
 	{
--- CrashReporter/Source/Resource.h
+++ CrashReporter/Source/Resource.h
@@ -3,16 +3,15 @@
 // Used by CrashReporter.rc
 //
 #define IDI_APP_ICON                    1
-#define IDD_MAIN                        101
-#define IDB_XLISTCTRL_CHECKBOXES        102
+#define IDB_XLISTCTRL_CHECKBOXES        101
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           103
+#define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
