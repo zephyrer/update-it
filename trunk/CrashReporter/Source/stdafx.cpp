621ca835bffbeca348805310f147b8772c5f0fc3
Elijah Zarezky <Elijah Zarezky@5557f4c3-8b41-0410-a5a5-81f158d74672>
2010-07-16 11:10:06 +0000

    Slightly reformatted source code.

--- trunk/CrashReporter/Source/stdafx.cpp
+++ trunk/CrashReporter/Source/stdafx.cpp
@@ -61,6 +61,8 @@ static char THIS_FILE[] = __FILE__;
 //////////////////////////////////////////////////////////////////////////////////////////////
 // manifest dependencies
 
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 // VC-2008 RTM                       9.0.21022.8
 // VC-2008 Feature Pack              9.0.30411.0
 // VC-2008 SP1                       9.0.30729.1
@@ -73,6 +75,5 @@ static char THIS_FILE[] = __FILE__;
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateCRT' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateMFC' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #endif   // _DEBUG
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 // end of file
