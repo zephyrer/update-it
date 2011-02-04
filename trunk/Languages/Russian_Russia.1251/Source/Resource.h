c868dddbd5f1a5f42bf04f2e8843b43e586e9855
Elijah Zarezky <Elijah Zarezky@5557f4c3-8b41-0410-a5a5-81f158d74672>
2011-02-04 14:46:32 +0000

    Continued implementing "browse for FTP folder" feature.

--- trunk/Languages/Russian_Russia.1251/Source/Resource.h
+++ trunk/Languages/Russian_Russia.1251/Source/Resource.h
@@ -143,6 +143,7 @@
 #define IDC_TEXT_EMAIL_INFO             1080
 #define IDC_EDIT_PASSWORD               1081
 #define IDC_BUTTON_FTP_ROOT             1082
+#define IDC_TREE_FTP_FOLDERS            1083
 #define ID_LANGUAGE_ENGLISH             0x9C50
 #define ID_LANGUAGE_RUSSIAN             0x9C60
 
@@ -152,7 +153,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        120
 #define _APS_NEXT_COMMAND_VALUE         40033
-#define _APS_NEXT_CONTROL_VALUE         1083
+#define _APS_NEXT_CONTROL_VALUE         1084
 #define _APS_NEXT_SYMED_VALUE           120
 #endif
 #endif
